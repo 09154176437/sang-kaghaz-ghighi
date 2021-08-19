17d1995e54aad58af418f5d7724e5535420b0f19
09154176437 <88371981+09154176437@users.noreply.github.com>
2021-08-19 16:47:24 +0430

    Add files via upload

---
+++ sang kaghaz ghiychi.py
@@ -0,0 +1,12 @@
+# import random
+# items =["sang","kaghaz","ghychi"]
+# computer_chice = random.choice(items)
+
+# print("1-rick")
+# print("2-paper")
+# print("3-scissor")
+# user_choice_index = int(input())
+# user_choice = items[user_choice_index]
+
+# if computer_choice == "rock" and user_choice == "scissor" :
+#     print("computer wins")
